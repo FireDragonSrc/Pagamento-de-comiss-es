819689459886021ad6024f0d2207b9c815f38f87
FireDragonSrc <firedragonsrc@gmail.com>
2023-10-17 11:18:34 -0300

    Commit inicial

---
+++ pagamentoComissao/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pagamentoComissao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o seu nome:");
+            string funcionario = Console.ReadLine();
+            Console.WriteLine("Qual o preço do produto?");
+            float produto = int.Parse(Console.ReadLine());
+            float resultado = 5/100f * produto;                  //5/100 X 700
+            Console.WriteLine("A sua comissão será de: " + resultado);
+            Console.ReadLine();
+        }
+    }
+}
